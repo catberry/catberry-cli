bdf19fb8c3f3c0cafcd779d310e7868f4566cf1e
Denis Rechkunov <mail@pragmader.me>
2014-09-18 14:56:57 +0700

    Add lazy loader for commits in example.

--- templates/example/browser.js
+++ templates/example/browser.js
@@ -1,9 +1,13 @@
 'use strict';
 
 var catberry = require('catberry'),
+	lazyLoader = require('catberry-lazy-loader'),
 	// this config will be replaced by `./config/browser.json` when building
 	// because of `browser` field in `package.json`
 	config = require('./config/environment.json'),
 	cat = catberry.create(config);
+
+// register lazy loader plugin to Catberry Service Locator
+lazyLoader.register(cat.locator);
 
 cat.startWhenReady();
--- templates/example/catberry_modules/commits/CommitsModule.js
+++ templates/example/catberry_modules/commits/CommitsModule.js
@@ -2,17 +2,33 @@
 
 module.exports = CommitsModule;
 
-var COMMITS_URL = 'https://api.github.com/repos/catberry/catberry/commits';
+var util = require('util');
+
+var COMMITS_URL = 'https://api.github.com/repos/catberry/catberry/commits',
+	COMMITS_PAGE_URL_FORMAT = COMMITS_URL + '?page=%d&per_page=%d',
+	PER_PAGE = 50;
 
 /**
  * Creates new instance of Commits module.
  * @param {UHR} $uhr Universal HTTP(S) request.
  * @param {jQuery} $jQuery jQuery library.
+ * @param {ServiceLocator} $serviceLocator Service locator to resolve plugin.
  * @constructor
  */
-function CommitsModule($uhr, $jQuery) {
+function CommitsModule($uhr, $jQuery, $serviceLocator) {
 	this._uhr = $uhr;
 	this.$ = $jQuery;
+	if (this.$context.isBrowser) {
+		this.lazyLoader = $serviceLocator.resolve('lazyLoader');
+		this.lazyLoader.containerId = 'commits-feed';
+		this.lazyLoader.loaderId = 'commits-loader';
+		this.lazyLoader.moreItemsCount = PER_PAGE;
+		this.lazyLoader.maxItemsCount = 10000;
+		this.lazyLoader.itemTemplateName = 'commits__item';
+		// factory to get next N items from data source
+		this.lazyLoader.factory =
+			CommitsModule.prototype.itemsFactory.bind(this);
+	}
 }
 
 /**
@@ -29,18 +45,71 @@ CommitsModule.prototype._uhr = null;
 CommitsModule.prototype.$ = null;
 
 /**
+ * Current lazy loader for infinite scroll.
+ * @type {LazyLoader}
+ * @private
+ */
+CommitsModule.prototype.lazyLoader = null;
+
+/**
+ * Current page number.
+ * @type {number}
+ * @private
+ */
+CommitsModule.prototype._page = 1;
+
+/**
  * Renders commit list of Catberry Framework repository.
  * This method is called when need to render "index" template
  * of module "commits".
  * @returns {Promise<Object>|Object|undefined} Data context.
  */
 CommitsModule.prototype.renderIndex = function () {
-	return this._uhr.get(COMMITS_URL)
+	return this.getItems(1, PER_PAGE)
+		.then(function (items) {
+			return {commits: items};
+		});
+};
+
+/**
+ * Does something after index placeholder is rendered.
+ * This method is invoked only in browser.
+ */
+CommitsModule.prototype.afterRenderIndex = function () {
+	this.lazyLoader.enableInfiniteScroll();
+};
+
+/**
+ * Current factory for feed items.
+ * @param {jQuery} last Last element in feed.
+ * @param {number} limit How many items to load.
+ * @returns {Promise<Array>} Promise for next chunk of items.
+ */
+CommitsModule.prototype.itemsFactory = function (last, limit) {
+	var self = this;
+	return this.getItems(this._page + 1, limit)
+		.then(function (items) {
+			self._page++;
+			return items;
+		});
+};
+
+/**
+ * Gets specified page of items.
+ * @param {number} page Page number.
+ * @param {number} limit Items count to load.
+ * @returns {Promise<Array>} Promise for items.
+ */
+CommitsModule.prototype.getItems = function (page, limit) {
+	return this._uhr.get(
+		util.format(COMMITS_PAGE_URL_FORMAT, page, limit)
+	)
 		.then(function (result) {
 			if (result.status.code >= 400 && result.status.code < 600) {
 				throw new Error(result.status.text);
 			}
-			return {commits: result.content};
+
+			return result.content;
 		});
 };
 
