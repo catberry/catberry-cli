e3bccfb3eebdb3e89d8d0d7bc4a53095bb7fd721
Denis Rechkunov <denis.rechkunov@gmail.com>
2015-02-10 18:23:15 +0600

    Add support for Catberry 4.0.

--- templates/empty/browser.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var catberry = require('catberry'),
-	// this config will be replaced by `./config/browser.json` when building
-	// because of `browser` field in `package.json`
-	config = require('./config/environment.json'),
-	cat = catberry.create(config);
-
-cat.startWhenReady();
