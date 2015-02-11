60b0972d6417a5221003e6ae58373f6f1b25f66f
Denis Rechkunov <denis.rechkunov@gmail.com>
2015-02-11 10:21:50 +0600

    Add Dust support.

---
+++ templates/empty-dust/catberry_stores/Main.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = Main;
+
+/*
+ * This is a Catberry Store file.
+ * More details can be found here
+ * https://github.com/catberry/catberry/blob/master/docs/index.md#stores
+ */
+
+/**
+ * Creates new instance of "Main" store.
+ * @constructor
+ */
+function Main() {
+
+}
+
+/**
+ * Loads data from somewhere.
+ * @returns {Object} Data object.
+ */
+Main.prototype.load = function () {
+	return {who: 'World'};
+};
