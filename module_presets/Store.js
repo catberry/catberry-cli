ed2bacbd6f6f20d210571da91cd804b5cd94da16
Denis Rechkunov <mail@pragmader.me>
2016-03-13 17:22:36 +0100

    Update module presets.

--- module_presets/Store.js
+++ module_presets/Store.js
@@ -1,31 +1,39 @@
 'use strict';
 
+/*
+ * This is a Catberry Cat-component file.
+ * More details can be found here
+ * http://catberry.org/documentation#stores-interface
+ */
+
 class __pascalName__ {
+
+	/**
+	 * Creates a new instance of the "__pascalName__" store.
+	 * @param {ServiceLocator} locator The service locator for resolving dependencies.
+	 */
 	constructor(locator) {
 
-		/**
-		* Current universal HTTP request to do it in isomorphic way.
-		* @type {UHR}
-		* @private
-		*/
-		this._uhr = locator.resolve('uhr');
+		// In case you have the UHR plugin registered
+		// this._uhr = locator.resolve('uhr');
 
 		/**
-		* Current lifetime of data (in milliseconds) that is returned by this store.
+		* Current lifetime for data (in milliseconds) which the store is responsible for.
 		* @type {number} Lifetime in milliseconds.
 		*/
 		this.$lifetime = 60000;
 	}
 
 	load() {
-		// Here you can do any HTTP requests using this._uhr.
+		// Here you can do any HTTP requests using this._uhr
+		// or another universal HTTP request implementation.
 		// Please read details here https://github.com/catberry/catberry-uhr.
 	}
 
 	handleSomeAction() {
 		// Here you can call this.$context.changed() if you know
-		// that remote data source has been changed.
-		// Also you can have many handle methods for other actions.
+		// that remote data has been changed.
+		// Also, you can have other actions and handle methods like this, just define a method.
 	}
 }
 
